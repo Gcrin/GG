19e45f0dae0da3c606a5f12d6db6f24b04f7c2b0
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-09-08 07:24:40 +0900

    feat(GGCore): GGCharacter 및 GGHeroComponent 클래스 추가
    플러그인의 캐릭터와 관련된 기본 클래스 및 컴포넌트를 추가하여 확장성을 제공.
    
    - `AGGCharacter` 클래스 생성 (`ALyraCharacter`를 상속)
    - `UGGHeroComponent` 클래스 생성 (`ULyraHeroComponent`를 상속)
    - 블루프린트에서 사용 가능한 `UGGHeroComponent` 설정 (`BlueprintSpawnableComponent` 지정)
    - 관련 헤더와 소스 파일 추가
    - `B_GG_Hero.uasset` 파일 업데이트

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Character/GGCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LyraCharacter.h"
+#include "GGCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGCORERUNTIME_API AGGCharacter : public ALyraCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Character/GGHeroComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/LyraHeroComponent.h"
+#include "GGHeroComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, Meta=(BlueprintSpawnableComponent))
+class GGCORERUNTIME_API UGGHeroComponent : public ULyraHeroComponent
+{
+	GENERATED_BODY()
+	
+};
