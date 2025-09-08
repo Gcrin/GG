ab72014ee901b764ae552f76447704664f39a54b
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-09-08 10:56:54 +0900

    feat(GGCore): GGGameplayAbility 및 GGResourceSet 클래스 추가
    능력 시스템의 확장성을 위해 GGGameplayAbility와 GGResourceSet 클래스를 구현.
    
    - `UGGGameplayAbility` 클래스 생성 (`ULyraGameplayAbility`를 상속)
    - `UGGResourceSet` 클래스 생성 (`ULyraAttributeSet`를 상속)
    - 관련 헤더 및 소스 파일 추가 (`GGGameplayAbility.h/cpp`, `GGResourceSet.h/cpp`)

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/AbilitySystem/Attributes/GGResourceSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Attributes/LyraAttributeSet.h"
+#include "GGResourceSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGCORERUNTIME_API UGGResourceSet : public ULyraAttributeSet
+{
+	GENERATED_BODY()
+	
+};
