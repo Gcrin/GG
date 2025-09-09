c97962483984198e47709eb8f67fdcebb6035b8f
taene <rlaxodus3445@naver.com>
2025-09-09 18:32:55 +0900

    feat(Attribute): GGUtilitySet 유틸리티 속성 추가 및 복제 로직 구현
    - UGGUtilitySet 클래스에 유틸리티 속성(스태미나/마나 회복 속도 등)과 관련 복제 로직을 추가.
    - 각 속성 변경 브로드캐스트 이벤트(On*Changed) 구현
    - 속성 복제를 위한 GetLifetimeReplicatedProps 로직 추가
    - 속성 변화 감지를 위한 OnRep_* 함수 및 RepNotify 설정 완료

--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/AbilitySystem/Attributes/GGUtilitySet.cpp
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/AbilitySystem/Attributes/GGUtilitySet.cpp
@@ -3,6 +3,55 @@
 
 #include "AbilitySystem/Attributes/GGUtilitySet.h"
 
+#include "Net/UnrealNetwork.h"
+
 UGGUtilitySet::UGGUtilitySet()
 {
+}
+
+void UGGUtilitySet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGUtilitySet, MoveSpeed, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGUtilitySet, StaminaRegenRate, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGUtilitySet, ManaRegenRate, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGUtilitySet, HealthRegenRate, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGUtilitySet, CooldownReduction, COND_OwnerOnly, REPNOTIFY_Always);
+}
+
+void UGGUtilitySet::OnRep_StaminaRegenRate(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGUtilitySet, StaminaRegenRate, OldValue);
+
+	OnStaminaRegenRateChanged.Broadcast(nullptr, nullptr, nullptr, GetStaminaRegenRate() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetStaminaRegenRate());
+}
+
+void UGGUtilitySet::OnRep_ManaRegenRate(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGUtilitySet, ManaRegenRate, OldValue);
+
+	OnManaRegenRateChanged.Broadcast(nullptr, nullptr, nullptr, GetManaRegenRate() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetManaRegenRate());
+}
+
+void UGGUtilitySet::OnRep_HealthRegenRate(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGUtilitySet, HealthRegenRate, OldValue);
+
+	OnHealthRegenRateChanged.Broadcast(nullptr, nullptr, nullptr, GetHealthRegenRate() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetHealthRegenRate());
+}
+
+void UGGUtilitySet::OnRep_MoveSpeed(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGUtilitySet, CooldownReduction, OldValue);
+
+	OnCooldownReductionChanged.Broadcast(nullptr, nullptr, nullptr, GetCooldownReduction() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetCooldownReduction());
+}
+
+void UGGUtilitySet::OnRep_CooldownReduction(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGUtilitySet, MoveSpeed, OldValue);
+
+	OnMoveSpeedChanged.Broadcast(nullptr, nullptr, nullptr, GetMoveSpeed() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetMoveSpeed());
 }
--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/AbilitySystem/Attributes/GGUtilitySet.h
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/AbilitySystem/Attributes/GGUtilitySet.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "AbilitySystemComponent.h"
 #include "AbilitySystem/Attributes/LyraAttributeSet.h"
 #include "GGUtilitySet.generated.h"
 
@@ -16,5 +17,55 @@ class GGCORERUNTIME_API UGGUtilitySet : public ULyraAttributeSet
 public:
 
 	UGGUtilitySet();
-	
+
+	ATTRIBUTE_ACCESSORS(UGGUtilitySet, StaminaRegenRate);
+	ATTRIBUTE_ACCESSORS(UGGUtilitySet, ManaRegenRate);
+	ATTRIBUTE_ACCESSORS(UGGUtilitySet, HealthRegenRate);
+	ATTRIBUTE_ACCESSORS(UGGUtilitySet, MoveSpeed);
+	ATTRIBUTE_ACCESSORS(UGGUtilitySet, CooldownReduction);
+
+	mutable FLyraAttributeEvent OnStaminaRegenRateChanged;
+	mutable FLyraAttributeEvent OnManaRegenRateChanged;
+	mutable FLyraAttributeEvent OnHealthRegenRateChanged;
+	mutable FLyraAttributeEvent OnMoveSpeedChanged;
+	mutable FLyraAttributeEvent OnCooldownReductionChanged;
+
+protected:
+
+	UFUNCTION()
+	virtual void OnRep_StaminaRegenRate(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_ManaRegenRate(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_HealthRegenRate(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MoveSpeed(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_CooldownReduction(const FGameplayAttributeData& OldValue);
+
+private:
+
+	// 초당 기력 회복 속도
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_StaminaRegenRate, Category = "GG|Utility", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData StaminaRegenRate;
+
+	// 초당 마력 회복 속도
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_ManaRegenRate, Category = "GG|Utility", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData ManaRegenRate;
+
+	// 초당 체력 회복 속도
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_HealthRegenRate, Category = "GG|Utility", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData HealthRegenRate;
+
+	// 이동 속도
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MoveSpeed, Category = "GG|Utility", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MoveSpeed;
+
+	// 스킬 쿨타임 감소 (예: 10.0 = 10% 감소)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_CooldownReduction, Category = "GG|Utility", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData CooldownReduction;
 };
