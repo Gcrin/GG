9f1c00b575f36b808a7ab57e6b4a52992975b1dd
taene <rlaxodus3445@naver.com>
2025-09-09 18:43:52 +0900

    feat(Character): GGCharacter에 GGResourceComponent 통합 및 초기화 로직 추가
    - GGCharacter에 GGResourceComponent를 통합하여 자원 관리 기능을 강화.
    - GGCharacter 생성자에 GGResourceComponent 생성 로직 추가
    - OnAbilitySystemInitialized에서 ResourceComponent 초기화 수행
      - LyraAbilitySystemComponent와의 상호작용 처리
    - OnAbilitySystemUninitialized에서 ResourceComponent의 정리 로직 추가

--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/Character/GGCharacter.cpp
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/Character/GGCharacter.cpp
@@ -3,3 +3,27 @@
 
 #include "Character/GGCharacter.h"
 
+#include "Character/GGResourceComponent.h"
+
+AGGCharacter::AGGCharacter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	ResourceComponent = CreateDefaultSubobject<UGGResourceComponent>(TEXT("ResourceComponent"));
+}
+
+void AGGCharacter::OnAbilitySystemInitialized()
+{
+	Super::OnAbilitySystemInitialized();
+
+	ULyraAbilitySystemComponent* LyraASC = GetLyraAbilitySystemComponent();
+	check(LyraASC);
+	
+	ResourceComponent->InitializeWithAbilitySystem(LyraASC);
+}
+
+void AGGCharacter::OnAbilitySystemUninitialized()
+{
+	Super::OnAbilitySystemUninitialized();
+
+	ResourceComponent->UninitializeFromAbilitySystem();
+}
