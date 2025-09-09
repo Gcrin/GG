7de38558d2190708e93f7b6cd019c082fa7c982f
taene <rlaxodus3445@naver.com>
2025-09-09 18:16:09 +0900

    feat(Attribute): GGDefenseSet 방어 속성 추가 및 복제 로직 구현
    - UGGDefenseSet에 물리/마법 피해 감소 및 흡수 속성 추가
    - 각 속성 변경 브로드캐스트 이벤트(On*Changed) 구현
    - 속성 복제를 위한 GetLifetimeReplicatedProps 로직 추가
    - 속성 변화 감지를 위한 OnRep_* 함수 및 RepNotify 설정 완료

--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/AbilitySystem/Attributes/GGDefenseSet.cpp
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/AbilitySystem/Attributes/GGDefenseSet.cpp
@@ -3,6 +3,50 @@
 
 #include "AbilitySystem/Attributes/GGDefenseSet.h"
 
+#include "Net/UnrealNetwork.h"
+
 UGGDefenseSet::UGGDefenseSet()
+	: PhysicalDamageReduction(0.0f)
+	, MagicDamageReduction(0.0f)
+	, PhysicalDamageAbsorption(0.0f)
+	, MagicDamageAbsorption(0.0f)
 {
+}
+
+void UGGDefenseSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGDefenseSet, PhysicalDamageReduction, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGDefenseSet, MagicDamageReduction, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGDefenseSet, PhysicalDamageAbsorption, COND_OwnerOnly, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UGGDefenseSet, MagicDamageAbsorption, COND_OwnerOnly, REPNOTIFY_Always);
+}
+
+void UGGDefenseSet::OnRep_PhysicalDamageReduction(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGDefenseSet, PhysicalDamageReduction, OldValue);
+	
+	OnPhysicalDamageReductionChanged.Broadcast(nullptr, nullptr, nullptr, GetPhysicalDamageReduction() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetPhysicalDamageReduction());
+}
+
+void UGGDefenseSet::OnRep_MagicDamageReduction(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGDefenseSet, MagicDamageReduction, OldValue);
+	
+	OnMagicDamageReductionChanged.Broadcast(nullptr, nullptr, nullptr, GetMagicDamageReduction() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetMagicDamageReduction());
+}
+
+void UGGDefenseSet::OnRep_PhysicalDamageAbsorption(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGDefenseSet, PhysicalDamageAbsorption, OldValue);
+	
+	OnPhysicalDamageAbsorptionChanged.Broadcast(nullptr, nullptr, nullptr, GetPhysicalDamageAbsorption() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetPhysicalDamageAbsorption());
+}
+
+void UGGDefenseSet::OnRep_MagicDamageAbsorption(const FGameplayAttributeData& OldValue)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UGGDefenseSet, MagicDamageAbsorption, OldValue);
+	
+	OnMagicDamageAbsorptionChanged.Broadcast(nullptr, nullptr, nullptr, GetMagicDamageAbsorption() - OldValue.GetCurrentValue(), OldValue.GetCurrentValue(), GetMagicDamageAbsorption());
 }
--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/AbilitySystem/Attributes/GGDefenseSet.h
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/AbilitySystem/Attributes/GGDefenseSet.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "AbilitySystemComponent.h"
 #include "AbilitySystem/Attributes/LyraAttributeSet.h"
 #include "GGDefenseSet.generated.h"
 
@@ -17,4 +18,45 @@ public:
 
 	UGGDefenseSet();
 	
+	ATTRIBUTE_ACCESSORS(UGGDefenseSet, PhysicalDamageReduction);
+	ATTRIBUTE_ACCESSORS(UGGDefenseSet, MagicDamageReduction);
+	ATTRIBUTE_ACCESSORS(UGGDefenseSet, PhysicalDamageAbsorption);
+	ATTRIBUTE_ACCESSORS(UGGDefenseSet, MagicDamageAbsorption);
+
+	mutable FLyraAttributeEvent OnPhysicalDamageReductionChanged;
+	mutable FLyraAttributeEvent OnMagicDamageReductionChanged;
+	mutable FLyraAttributeEvent OnPhysicalDamageAbsorptionChanged;
+	mutable FLyraAttributeEvent OnMagicDamageAbsorptionChanged;
+
+protected:
+
+	UFUNCTION()
+	virtual void OnRep_PhysicalDamageReduction(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MagicDamageReduction(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_PhysicalDamageAbsorption(const FGameplayAttributeData& OldValue);
+
+	UFUNCTION()
+	virtual void OnRep_MagicDamageAbsorption(const FGameplayAttributeData& OldValue);
+
+private:
+
+	// 물리 피해 감소 (예: 10.0 = 10% 감소)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_PhysicalDamageReduction, Category = "GG|Defense", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData PhysicalDamageReduction;
+
+	// 마법 피해 감소 (예: 10.0 = 10% 감소)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MagicDamageReduction, Category = "GG|Defense", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MagicDamageReduction;
+
+	// 물리 피해 흡수 (받은 물리 피해의 일부를 체력으로 전환, 예: 5.0 = 5% 흡수)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_PhysicalDamageAbsorption, Category = "GG|Defense", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData PhysicalDamageAbsorption;
+
+	// 마법 피해 흡수 (받은 마법 피해의 일부를 체력으로 전환, 예: 5.0 = 5% 흡수)
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MagicDamageAbsorption, Category = "GG|Defense", Meta = (AllowPrivateAccess = true))
+	FGameplayAttributeData MagicDamageAbsorption;
 };
