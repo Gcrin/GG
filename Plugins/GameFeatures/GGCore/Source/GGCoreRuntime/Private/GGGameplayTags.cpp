0fe79738316137ebf65b6a673ccc75588a575839
taene <rlaxodus3445@naver.com>
2025-09-10 16:16:10 +0900

    feat(GGCore): GGGameplayTags 선언 및 정의 추가
    GGCoreRuntime에 공통적으로 사용할 GGGameplayTags를 선언 및 정의하는 모듈 추가.
    
    - `Data_Damage_Physical`, `Data_Damage_Magic` 등의 태그 선언 및 정의
    - `Ability_SkillDamage`, `Ability_SkillDamage_Multiplier` 태그 포함
    - 기능 확장을 위한 네임스페이스 구조화

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/GGGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GGGameplayTags.h"
+
+namespace GGGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Data_Damage_Physical, "Data.Damage.Physical", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Data_Damage_Magic, "Data.Damage.Magic", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_SkillDamage, "Ability.SkillDamage", "");
+	UE_DEFINE_GAMEPLAY_TAG_COMMENT(Ability_SkillDamage_Multiplier, "Ability.SkillDamage.Multiplier", "");
+}
---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/GGGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GGGameplayTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Damage_Physical);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Data_Damage_Magic);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_SkillDamage);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Ability_SkillDamage_Multiplier);
+};
