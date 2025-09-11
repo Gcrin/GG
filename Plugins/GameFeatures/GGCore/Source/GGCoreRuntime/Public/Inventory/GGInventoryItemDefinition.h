d9261f51fdc47a7a02f636940f51f7aada996b83
Eldia <nsw22122@gmail.com>
2025-09-11 21:12:39 +0900

    feat(GGCore): Inventory폴더 추가 및 GGInventoryItemDefinition 클래스 생성, 로직 구현

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Inventory/GGInventoryItemDefinition.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GGInventoryItemDefinition.generated.h"
+
+UCLASS(Abstract, DefaultToInstanced, EditInlineNew)
+class UGGInventoryItemFragment : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+};
+
+/**
+ * 
+ */
+UCLASS()
+class GGCORERUNTIME_API UGGInventoryItemDefinition : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UGGInventoryItemDefinition(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	
+	/* Inventory Item 정의(메타) 이름 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Display)
+	FText DisplayName;
+	
+	/* 아이템이 어떤 특성(Fragment)들로 구성되어 있는지 담는 배열 */
+	UPROPERTY(EditDefaultsOnly, Instanced, BlueprintReadOnly, Category = Display)
+	TArray<TObjectPtr<UGGInventoryItemFragment>> Fragments;
+};
