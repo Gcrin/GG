4e2fdd391294065e666b4043b93b582cd3236c21
Eldia <nsw22122@gmail.com>
2025-09-11 18:39:33 +0900

    feat(Weapons): GGRangedWeaponInstance 거리가 있는 무기용 인스턴스 클래스 추가

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Weapons/GGRangedWeaponInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapons/GGWeaponInstance.h"
+#include "GGRangedWeaponInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGCORERUNTIME_API UGGRangedWeaponInstance : public UGGWeaponInstance
+{
+	GENERATED_BODY()
+	
+};
