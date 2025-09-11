85e20aa91fce22471de440a87160df2b58a5a6b2
Eldia <nsw22122@gmail.com>
2025-09-11 18:36:17 +0900

    feat(GGCore): Equipment 폴더 및 GGEquipmentInstance 추가

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Equipment/GGEquipmentInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GGEquipmentInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGCORERUNTIME_API UGGEquipmentInstance : public UObject
+{
+	GENERATED_BODY()
+	
+};
