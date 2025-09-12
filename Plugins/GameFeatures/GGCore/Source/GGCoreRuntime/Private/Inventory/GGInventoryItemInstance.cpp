f1f9e3385242951d4cf84a3fa1d75eb7b3f2373d
Eldia <nsw22122@gmail.com>
2025-09-12 16:13:19 +0900

    feat(Inventory): GGInventoryItemInstance 생성 및 로직 추가

---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/Inventory/GGInventoryItemInstance.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Inventory/GGInventoryItemInstance.h"
+
+UGGInventoryItemInstance::UGGInventoryItemInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
