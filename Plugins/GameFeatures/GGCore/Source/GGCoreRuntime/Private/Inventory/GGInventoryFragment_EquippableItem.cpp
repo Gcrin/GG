7267018705c6bb78fd557e662fa30c069368900b
Eldia <nsw22122@gmail.com>
2025-09-12 16:00:45 +0900

    feat(Inventory): GGInventoryFragment_EquippableItem 로직 수정

--- Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/Inventory/GGInventoryFragment_EquippableItem.cpp
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Private/Inventory/GGInventoryFragment_EquippableItem.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Inventory/GGInventoryItemFragment_EquippableItem.h"
+#include "Inventory/GGInventoryFragment_EquippableItem.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(GGInventoryFragment_EquippableItem)
---
+++ Plugins/GameFeatures/GGCore/Source/GGCoreRuntime/Public/Inventory/GGInventoryFragment_EquippableItem.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Inventory/GGInventoryItemDefinition.h"
+#include "Templates/SubclassOf.h"
+#include "GGInventoryFragment_EquippableItem.generated.h"
+
+UCLASS()
+class UGGInventoryFragment_EquippableItem : public UGGInventoryItemFragment
+{
+	GENERATED_BODY()
+	
+};
